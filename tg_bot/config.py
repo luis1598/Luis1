d7845ae58fa9f22471b9dad7eeb2fe8ce2203fab
luis1598 <85584374+luis1598@users.noreply.github.com>
2022-01-29 22:11:51 +0100

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -1,3 +1,8 @@
+if not __name__.endswith("sample_config"):
+    import sys
+    print("The README is there to be read. Extend this sample config to a config file, don't just rename and change "
+          "values here. Doing that WILL backfire on you.\nBot quitting.", file=sys.stderr)
+    quit(1)
 class Config(object):
     LOGGER = True
 
